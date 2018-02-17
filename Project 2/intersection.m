9f1796a633aab0cbf44b035e8f8ceef193c06c4c
tomdjarvinen <tomdjarvinen@gmail.com>
2018-02-16 23:07:26 -0500

    Update intersection.m

--- Project 2/intersection.m
+++ Project 2/intersection.m
@@ -6,8 +6,8 @@ C = zeros(N);
 
 for x = 1:N(1)
     for y = 1:N(2)
-        if(A(x,y) == 1 && B(x,y)) == 1
-            C(x,y) = A(x,y);
+        if (A(x,y) == 1 && B(x,y)) == 1)
+            C(x,y) = 1;
         end
     end
 end
