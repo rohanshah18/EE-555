d448b7f1d9009711bf68b07b29fad3f09ab2f130
tomdjarvinen <tomdjarvinen@gmail.com>
2018-04-06 17:36:30 -0400

    Add files via upload

--- EE555 P4/gaborFilter.m
+++ EE555 P4/gaborFilter.m
@@ -1,6 +1,9 @@
 function [ Ifilt ] = gaborFilter(Image,F,theta,sigma)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%   Perform gaborFiltering on the input Image
+%   Inputs: Image: image to be modified
+%   	F: pixel frequency for filter
+%	theta: angle for filter in degrees
+%	sigma: variance for filter
     GEF = gefGenerator(F,theta,sigma);
     Ifilt = convolve2D(Image,GEF);
 end
